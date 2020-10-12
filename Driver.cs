95a571f380fc7a49bbd92f7d7b7a902ccf5c7d00
Burim S <buli.s166@gmail.com>
2020-10-12 16:19:49 -0400

    Add files via upload

---
+++ Driver.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+using System.Linq;
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantumEncryptionSeed
+{
+    class Driver
+    {
+        static string bitgenerator(){
+             using (var sim = new QuantumSimulator())
+            {                
+                var output = RandomQuantumBitGenerator.Run(sim).Result == Result.One ? "1" : "0";
+                
+                return output;  // Return the result          
+            }
+        }
+
+        static string bitstring() {
+            string bits = "";
+            for (int i=0; i<=128; i++){
+                bits += bitgenerator();
+            }
+            Console.WriteLine(bits);
+            return bits;
+        }
+
+
+        private static byte[] GetBytes(string bitString)
+        {
+             byte[] result = Enumerable.Range(0, bitString.Length / 8).
+                 Select(pos => Convert.ToByte(
+                   bitString.Substring(pos * 8, 8), 2)).ToArray();
+
+                  List<byte> myByteArray = new List<byte>();
+                      for (int i = result.Length - 1; i >= 0; i--)
+                      {
+                       myByteArray.Add(result[i]);
+                      }
+
+            return myByteArray.ToArray();
+        }
+      
+        static void Main(string[] args)
+        {
+           Console.WriteLine(Convert.ToBase64String(GetBytes(bitstring())));
+           
+         }
+      }
+    }
---
+++ QSeedGenerator.qs
@@ -0,0 +1,19 @@
+namespace QuantumEncryptionSeed {
+    open Microsoft.Quantum.Convert;
+    open Microsoft.Quantum.Math;
+    open Microsoft.Quantum.Measurement;
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Intrinsic;
+    
+    @EntryPoint()
+    operation RandomQuantumBitGenerator() : Result {
+        using (q = Qubit())  {  // Allocate a qubit.
+            H(q);               // Put the qubit to superposition. It now has a 50% chance of being 0 or 1.
+            return MResetZ(q);  // Measure the qubit value.
+        }
+    }
+
+    
+   
+
+}
